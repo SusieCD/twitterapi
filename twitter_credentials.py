7b60db468115e9fcc6e30a1863496765ac616249
test <test@gmail.com>
2021-06-28 14:28:52 +1000

    copied twitter stream api py code

---
+++ twitter_credentials.py
@@ -0,0 +1,7 @@
+# YouTube Video: https://www.youtube.com/watch?v=wlnx-7cm4Gg
+# Variables that contains the user credentials to access Twitter API 
+
+ACCESS_TOKEN = "1019729563574624256-kdoFtpCj9P72zcy9SCyU1xLocjfeKW"
+ACCESS_TOKEN_SECRET = "nDqhbC8Ofcn63GSUdsBnJdb0BbS45Aku78P70cfdhVBxT"
+CONSUMER_KEY = "xy3M4v47HoS3ReS3X47UedgdB"
+CONSUMER_SECRET = "R1sl6MY0TdtrBIPVmQ70CfjTj5bMocNbKmuQXsdfEKJgneNjMW"
